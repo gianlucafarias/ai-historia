dfb5301d1a635a3b49bdb7918f09dd95dc514e24
gianlucafarias <palmiergianluca@gmail.com>
2024-04-12 11:58:40 -0300

    fix type any

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,7 +49,7 @@ function Home() {
     setText(question);
   };
 
-  const shuffleArray = (array) => {
+  const shuffleArray = (array: any) => {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
